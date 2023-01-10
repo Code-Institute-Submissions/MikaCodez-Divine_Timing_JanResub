0ab818caee045781eca19478d0aa2b45a6fcd639
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2023-01-10 17:09:59 +0000

    trying to retrieve data

--- divine_timing/settings.py
+++ divine_timing/settings.py
@@ -127,9 +127,9 @@ WSGI_APPLICATION = 'divine_timing.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # if 'DATABASE_URL' in os.environ:
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#         }
 # else:
 #     DATABASES = {
 #         'default': {
@@ -138,6 +138,12 @@ DATABASES = {
 #         }
 #     }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
