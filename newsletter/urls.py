7a03013ce71dfa99caddcb1eb536f99987b1cfb3
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2023-01-19 13:07:49 +0000

    testing newsletter subscription issues to see if I can receieve emails successfully and successfully subscribe new users 2

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('subscriber', views.subscribers, name='subscriber'),
-    path('add_subscriber', views.add_subscriber, name='add_subscriber'),
-    path('unsubscribe', views.unsubscribe, name='unsubscribe'),
-    path('unsubscribe_registered_user', views.unsubscribe_registered_user,
-         name='unsubscribe_registered_user'),
+    path('subscriber', views.subscriber_signup, name='subscriber'),
 ]
